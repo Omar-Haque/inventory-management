bd1154f14ffedf0eb594c737f9eb85efe56489d0
Omar Abbas Haque <Abbas@Omars-MacBook-Air.local>
2024-08-06 00:20:06 +0530

    Design improved

--- app/page.js
+++ app/page.js
@@ -6,7 +6,9 @@ import {
   Modal,
   TextField,
   Button,
+  List,
 } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import {
@@ -31,10 +33,21 @@ const item = [
   "kale",
 ];
 
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+const theme = createTheme({
+  palette: {
+    color1: createColor("#292727"),
+  },
+});
+
 export default function Home() {
+  // States
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState("");
+  const [search, setSearch] = useState("");
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, "inventory"));
@@ -47,7 +60,6 @@ export default function Home() {
       });
     });
     setInventory(inventoryList);
-    console.log(inventoryList);
   };
 
   const addItem = async (item) => {
@@ -92,6 +104,7 @@ export default function Home() {
       alignItems={"center"}
       gap={2}
       flexDirection="column"
+      bgcolor={"#111111"}
     >
       <Modal open={open} onClose={handleClose}>
         <Box
@@ -99,23 +112,45 @@ export default function Home() {
           top="50%"
           left="50%"
           width="400"
-          bgcolor="white"
+          bgcolor="#393433"
           border="2px solid #000"
           boxShadow={24}
           p={4}
           display="flex"
           flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
           gap={3}
+          borderRadius={3}
           sx={{
             transform: "translate(-50%, -50%)",
           }}
         >
-          <Typography variant="h6">Add Item</Typography>
+          <Typography variant="h5" color={"white"}>
+            ADD ITEM
+          </Typography>
           <Stack width={"100%"} direction={"row"} spacing={2}>
             <TextField
-              variant="outlined"
-              fullWidth
-              value={itemName}
+              padding={3}
+              label="Item Name"
+              variant="filled"
+              style={{ minWidth: "400px" }}
+              sx={{
+                // Root class for the input field
+                "& .MuiFilledInput-root": {
+                  color: "white",
+                  fontFamily: "Arial",
+                  fontWeight: "bold",
+                  backgroundColor: "#f4f4f4",
+                  borderTopLeftRadius: "7px",
+                  borderTopRightRadius: "7px",
+                },
+                // Class for the label of the filled input field
+                "& .MuiInputLabel-filled": {
+                  color: "black",
+                  fontWeight: "bold",
+                },
+              }}
               onChange={(e) => {
                 setItemName(
                   e.target.value.charAt(0).toUpperCase() +
@@ -124,7 +159,10 @@ export default function Home() {
               }}
             />
             <Button
-              variant="outlined"
+              variant="contained"
+              style={{
+                backgroundColor: "#292727",
+              }}
               onClick={() => {
                 addItem(itemName);
                 setItemName("");
@@ -136,56 +174,133 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      {/* <Typography variant="h1" fontWeight="medium">
-        Inventory Management
-      </Typography> */}
-      <Button variant="contained" onClick={() => handleOpen()}>
-        Add new item
-      </Button>
-      <Box border="1px solid #333">
-        <Box
-          width="800px"
-          height="100px"
-          bgcolor="#ADD8E6"
-          display="flex"
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
-          <Typography variant="h2" colour="#333">
-            Inventory Items
-          </Typography>
-        </Box>
+      <Stack
+        bgcolor="#393433"
+        padding={10}
+        display={""}
+        justifyContent={"center"}
+        alignItems={"center"}
+        borderRadius={10}
+        gap={4}
+        sx={{
+          transform: "scale(0.6)",
+        }}
+      >
+        <Typography variant="h1" fontWeight="medium" color="white">
+          Inventory Management
+        </Typography>
 
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          {inventory.map(({ name, quantity }) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              bgcolor="white"
-              padding={5}
+        <Box>
+          <Box
+            width="1000px"
+            height="100px"
+            bgcolor=""
+            display="flex"
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            padding={4}
+            margin={5}
+          >
+            <TextField
+              label={"Search Item"}
+              padding={3}
+              variant="filled"
+              style={{ minWidth: "400px" }}
+              inputProps={{ style: { fontSize: 40 } }}
+              InputLabelProps={{ style: { fontSize: 20 } }}
+              sx={{
+                // Root class for the input field
+                "& .MuiFilledInput-root": {
+                  color: "white",
+                  fontFamily: "Arial",
+                  fontWeight: "bold",
+                  backgroundColor: "#f4f4f4",
+                  borderTopLeftRadius: "7px",
+                  borderTopRightRadius: "7px",
+                },
+                // Class for the label of the filled input field
+                "& .MuiInputLabel-filled": {
+                  color: "black",
+                  fontWeight: "bold",
+                },
+              }}
+              onChange={(e) => setSearch(e.target.value)}
+            ></TextField>
+            <Button
+              borderRadius={3}
+              style={{
+                backgroundColor: "#292727",
+                minWidth: "400px",
+                minHeight: "100px",
+              }}
+              variant="contained"
+              onClick={() => handleOpen()}
             >
-              <Typography variant="h3" color="#933" textAlign="center">
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </Typography>
-              <Typography variant="h3" color="#333" textAlign="center">
-                {quantity}
-              </Typography>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  removeItem(name);
-                }}
-              >
-                Remove
-              </Button>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
+              <Typography variant="h4">Add new item</Typography>
+            </Button>
+          </Box>
+
+          <Stack
+            id="PROBLEM"
+            width="900px"
+            height="400px"
+            spacing={0}
+            overflow="auto"
+            sx={{
+              // Center the Stack within its container
+              margin: "0 auto",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+            }}
+          >
+            {inventory
+              .filter((name) => {
+                return search.toLowerCase() === ""
+                  ? name
+                  : name.name.toLowerCase().includes(search);
+              })
+              .map(({ name, quantity }) => (
+                <Box
+                  width={"100%"}
+                  minHeight={"100px"}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  bgcolor="#57504C"
+                  padding={5}
+                  borderRadius={5}
+                  border={"2px solid #000"}
+                >
+                  <Box
+                    width="100%"
+                    display={"flex"}
+                    justifyContent={"space-between"}
+                    padding={4}
+                  >
+                    <Typography variant="h3" color="white" textAlign="center">
+                      {name.charAt(0).toUpperCase() + name.slice(1)}
+                    </Typography>
+                    <Typography variant="h3" color="white" textAlign="center">
+                      {quantity}
+                    </Typography>
+                  </Box>
+                  <Button
+                    variant="contained"
+                    style={{
+                      backgroundColor: "#292727",
+                    }}
+                    display="flex"
+                    onClick={() => {
+                      removeItem(name);
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </Box>
+              ))}
+          </Stack>
+        </Box>
+      </Stack>
     </Box>
   );
 }
